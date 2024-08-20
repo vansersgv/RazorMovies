3a2fbd12689ca7279e714b0903d479f6eb303293
sergio vergara <vansersgv@gmail.com>
2024-08-20 10:41:22 -0300

    Agregar archivos de proyecto.

---
+++ RazorLibrary/Data/RazorMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorLibrary.Models;
+
+namespace RazorMovies.Data
+{
+    public class RazorMoviesContext : DbContext
+    {
+        public RazorMoviesContext (DbContextOptions<RazorMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorLibrary.Models.Movie> Movie { get; set; } = default!;
+    }
+}
